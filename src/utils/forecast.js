af6b72f3ef38761463160238e886e86fd815eed4
Madefor Testonly <greerherher@gmail.com>
2019-09-06 12:46:28 +0430

    Updated forecast text to unclude high and low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. With a high temperature of ${body.daily.data[0].temperatureHigh} and a low temperature of ${body.daily.data[0].temperatureLow}`)
         }
     })
 }
